44d5d95f2f3287300c8964b5718aea57028c2fc5
Stefan <sciausu@outlook.com>
2025-02-05 11:09:05 -0500

    open steam lib
    needs public library to download or login to steam

--- Code/SGH.js
+++ Code/SGH.js
@@ -1,12 +1,28 @@
+var steamID = null; // null for now
+
 function testing()
 {
     var listLen = 20;
     var out = document.getElementById("test");
-    
+
     for (var i = 0; i < listLen; i++)
     {
         out.innerHTML += "test added";
         out.innerHTML += "</br>"; // does the same thing as "/n"
     }
     
+}
+
+function setSteamID()
+{
+    steamID = document.getElementById("steamid").value;
+    loadSteamGameLib();
+}
+
+function loadSteamGameLib()
+{
+    alert("test");
+    const steamLIB = "https://steamcommunity.com/profiles/"+steamID+"/games/?tab=all";
+    alert(steamLIB);
+    window.open(steamLIB, "_Blank");
 }
