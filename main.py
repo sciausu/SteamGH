7bf4ba2f8828f8ff1ed493b47936db98ec484055
Stefan <sciausu@outlook.com>
2025-02-12 15:42:53 -0500

    test added + licenses

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from steam_web_api import Steam
+from howlongtobeatpy import HowLongToBeat
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -13,6 +14,14 @@ print("Steam ID: ", steamID)
 
 steam = Steam(key)
 
+
+tmpSearch = "Kingdom Come: Deliverance"
+game = HowLongToBeat().search(tmpSearch)
+if game is not None and len(game) > 0:
+    best_element = max(game, key=lambda element: element.similarity)
+print("Main Story:", best_element.main_story) # returns float, round for final build
+print("Main + Extras:", best_element.main_extra)
+print("Completionist:", best_element.completionist)
 
 # arguments: steamid
 #user = steam.users.get_owned_games(steamID)
