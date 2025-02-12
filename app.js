9f283382f34bc9f1884fa4497ce77c1dc57f67bf
Stefan <sciausu@outlook.com>
2025-02-12 15:08:53 -0500

    oooohhhh working on it

--- app.js
+++
@@ -1,3 +0,0 @@
-// file has to be called spp.js to create association
-
-const express = require("express");
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import requests
-import json
 import os
+from steam_web_api import Steam
 
 from dotenv import load_dotenv
 load_dotenv()
 
-steamApiKey = os.getenv("STEAM_API_KEY")
-print(steamApiKey)
+# modify / create a .env file and add associated values
+steamID = os.getenv("STEAM_ID")
+KEY = os.getenv("STEAM_API_KEY")
+
+print("API key: ", KEY)
+print("Steam ID: ", steamID)
+
+steam = Steam(KEY)
+
+
+# arguments: steamid
+#user = steam.users.get_owned_games(steamID)
